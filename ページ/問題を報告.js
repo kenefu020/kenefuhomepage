513a0802cf663b396b6b3af80ffe1cbb1bd48dc7
kenefu020 <kenefu020@gmail.com>
2024-10-15 00:55:24 +0900

    保存

--- ページ/問題を報告.js
+++ ページ/問題を報告.js
@@ -1,6 +1,16 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log(input.value);
+    this.addEventListener();
 });
+
+function add() {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+}
