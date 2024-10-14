129e99fe75e11d1f8b1c9ccd9717597a3f4c573c
kenefu020 <kenefu020@gmail.com>
2024-10-15 00:43:29 +0900

    保存

--- ページ/問題を報告.js
+++ ページ/問題を報告.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
     console.log(input.value);
 });
