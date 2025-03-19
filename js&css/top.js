a571af910dce9245e05ac0daad7e11c8f5fb000d
kenefu020 <kenefu020@gmail.com>
2025-03-19 21:40:18 +0900

    update

---
+++ js&css/top.js
@@ -0,0 +1,13 @@
+let header = document.getElementById('header')
+let html = 
+(
+    "<div class='top'><br><h1 class='text'>"
+    +"<a class='b' href='../../../'>TOP</a> "
+    +"<a class='b' href='../../page/sns/'>SNS</a> "
+    +"<a class='b' href='../../page/other'>OTHER</>"
+    +"</h1></div>");
+header.innerHTML = html;
+let a = (1)
+console.log('正常に動作しました。');
+
+console.log('今すぐこの画面を閉じてください')
